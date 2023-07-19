d7c20d263bb213468067d290d6d02c58dd74ccc7
Joseph Aguayo <137316260+joejay22@users.noreply.github.com>
2023-07-19 13:11:30 -0700

    Add files via upload

---
+++ Video Game Sales historical data.sql
@@ -0,0 +1,68 @@
+//*In This Project I clean and prepare data, I then go into a exploratory analysis examing multiple trends
+the main component of this Project was joining tables*/
+
+ --Cleaning and prepping the data, platform is null for the 2600 we will fix it up with a quick update command
+UPDATE dbo.vgsalesTitles
+		SET Platform = 2600 where Platform is NULL;
+--Under year there are null values however I got an error trying to update in "Null" as the columns is a integer not Varhar 
+ /* UPDATE dbo.vgsalesTitles
+		SET year = 'N/A' where year is NULL; */
+SELECT *
+FROM dbo.vgsalesTitles 
+join dbo.vgsales$
+on dbo.vgsales$.Rank = dbo.vgsalesTitles.rank
+ORDER BY platform;
+
+--Lets check how many games each platform has using a Count function
+SELECT Platform, COUNT(Platform) as 'titles on platform'
+FROM dbo.vgsalesTitles
+join dbo.vgsales$
+ON dbo.vgsales$.Rank = dbo.vgsalesTitles.Rank
+GROUP BY Platform
+ORDER BY 'Titles on Platform' DESC
+--Now lets check the global sales per platform
+SELECT Platform, sum(Global_Sales) AS "GLOBAL SALES"
+FROM dbo.vgsalesTitles
+join dbo.vgsales$
+ON dbo.vgsales$.Rank = dbo.vgsalesTitles.Rank
+GROUP BY Platform
+ORDER BY "GLOBAL SALES" DESC;
+
+--This allows us to see the Name, Platform, Year and global sales ordered by sales for the top 100 games
+SELECT Name, platform, Year, Global_Sales
+FROM dbo.vgsalesTitles
+join dbo.vgsales$
+ON dbo.vgsales$.Rank = dbo.vgsalesTitles.Rank
+WHERE dbo.vgsalesTitles.Rank <= 100
+ORDER by Global_Sales desc
+
+--Number of games in each genre
+SELECT Genre, COUNT(Name) as 'Games per Genre'
+FROM dbo.vgsalesTitles
+GROUP BY Genre
+Order by 'Games per Genre' desc;
+
+--Sales per Genre
+SELECT Genre, COUNT(Global_Sales) as 'Global Sales'
+FROM dbo.vgsalesTitles
+join dbo.vgsales$
+ON dbo.vgsales$.Rank = dbo.vgsalesTitles.Rank
+GROUP BY Genre
+ORDER BY 'Global Sales' DESC
+
+--Popularity by Genre see how it differs from NA, EU and JP
+SELECT Genre, SUM(NA_Sales) as 'North America Popularity', SUM(EU_Sales) 'Europe Popularity', SUM(JP_Sales) 'Japan Popularity', SUM(Global_Sales) as 'Global Popularity'
+FROM dbo.vgsalesTitles
+join dbo.vgsales$
+ON dbo.vgsales$.Rank = dbo.vgsalesTitles.Rank
+WHERE dbo.vgsalesTitles.Rank <= 100
+GROUP BY Genre
+ORDER BY 'Global Popularity' desc
+
+--sales split by year
+SELECT Year, sum(Global_Sales) AS "GLOBAL SALES"
+FROM dbo.vgsalesTitles
+join dbo.vgsales$
+ON dbo.vgsales$.Rank = dbo.vgsalesTitles.Rank
+GROUP BY Year
+ORDER BY Year
