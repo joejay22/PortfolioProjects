c8824034709a20c137cc32d603d21e4b1c212956
Joseph Aguayo <137316260+joejay22@users.noreply.github.com>
2023-07-26 20:10:59 -0700

    Add files via upload

--- Nashville Housing Data -Data Cleaning.sql
+++ Nashville Housing Data -Data Cleaning.sql
@@ -1,16 +1,16 @@
 
 --Cleaning Data in SQL 
 Select * 
-from dbo.NashvileHousing;
+from dbo.NashvilleHousing;
 
 
 ------------------------------------------------------------------------------------------------------------------
 --Standardize Date Format using Convert
 
 Select SaleDate, CONVERT(Date,SaleDate)
-from dbo.NashvileHousing
+from dbo.NashvilleHousing
 
-Update NashvileHousing
+Update NashvilleHousing
 Set SaleDate = Convert(Date,SaleDate)
 
 
@@ -18,22 +18,22 @@ Set SaleDate = Convert(Date,SaleDate)
 --Pupulate Property Address data 
 
 Select *
-from dbo.NashvileHousing
+from dbo.NashvilleHousing
 where PropertyAddress is null
 --order by ParcelID
 
 --Parcel ID matches property address to fill in Nulls we will join in the tables 
 Select a.ParcelID, a.PropertyAddress, B.ParcelID, B.PropertyAddress, ISNULL (a.PropertyAddress, b.PropertyAddress)
-From dbo.NashvileHousing a 
-join dbo.NashvileHousing b
+From dbo.NashvilleHousing a 
+join dbo.NashvilleHousing b
 	on a.ParcelID = b.ParcelID
 	and a.[UniqueID] <> b.[UniqueID]
 Where a.PropertyAddress is null 
 
 UPDATE a
 SET PropertyAddress = ISNULL (a.PropertyAddress, b.PropertyAddress)
-From dbo.NashvileHousing a 
-join dbo.NashvileHousing b
+From dbo.NashvilleHousing a 
+join dbo.NashvilleHousing b
 	on a.ParcelID = b.ParcelID
 	and a.[UniqueID] <> b.[UniqueID]
 Where a.PropertyAddress is null
@@ -43,59 +43,59 @@ Where a.PropertyAddress is null
 --Organizing Address Into Individual Columns (Address, City, State)
 
 Select PropertyAddress
-from dbo.NashvileHousing
+from dbo.NashvilleHousing
 
 Select 
 SUBSTRING(PropertyAddress, 1, CHARINDEX(',',PropertyAddress)-1) as Address,
 	SUBSTRING(PropertyAddress, CHARINDEX(',',PropertyAddress)+1, LEN(PropertyAddress)) as Address 
-from dbo.NashvileHousing
+from dbo.NashvilleHousing
 
 
-ALTER TABLE NashvileHousing
+ALTER TABLE NashvilleHousing
 add PropertySplitAddress Nvarchar(255);
 
-Update NashvileHousing
+Update NashvilleHousing
 Set PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',',PropertyAddress)-1)
 
 
-ALTER TABLE NashvileHousing
+ALTER TABLE NashvilleHousing
 ADD PropertySplitCity Nvarchar(255)
 
-Update NashvileHousing
+Update NashvilleHousing
 Set PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',',PropertyAddress)+1, LEN(PropertyAddress))
 
 /* Select*
-from dbo.NashvileHousing */
+from dbo.NashvilleHousing */
 --Now I will update the owner address that are null
 
 Select Owneraddress
-from dbo.NashvileHousing
+from dbo.NashvilleHousing
 
 
 Select 
 PARSENAME(REPLACE(OwnerAddress,',','.') ,3),
 PARSENAME(REPLACE(OwnerAddress,',','.') ,2),
 PARSENAME(REPLACE(OwnerAddress,',','.') ,1)
-from dbo.NashvileHousing
+from dbo.NashvilleHousing
 
-ALTER TABLE NashvileHousing
+ALTER TABLE NashvilleHousing
 add OwnerSplitAddress Nvarchar(255);
 
-Update NashvileHousing
+Update NashvilleHousing
 Set OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress,',','.') ,3)
 
 
-ALTER TABLE NashvileHousing
+ALTER TABLE NashvilleHousing
 ADD OwnerSplitCity Nvarchar(255)
 
-Update NashvileHousing
+Update NashvilleHousing
 Set OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress,',','.') ,2)
 
 
-ALTER TABLE NashvileHousing
+ALTER TABLE NashvilleHousing
 ADD OwnerSplitState Nvarchar(255)
 
-Update NashvileHousing
+Update NashvilleHousing
 Set OwnerSplitState = PARSENAME(REPLACE(OwnerAddress,',','.') ,1)
 
 ---------------------------------------------------------------------------------------------------------------------------------------
@@ -103,7 +103,7 @@ Set OwnerSplitState = PARSENAME(REPLACE(OwnerAddress,',','.') ,1)
 --Changing Y and N to yes and no in Sold as Vacant
 
 Select Distinct(SoldAsVacant)
-from dbo.NashvileHousing
+from dbo.NashvilleHousing
 
 
 Select SoldAsVacant,
@@ -112,9 +112,9 @@ Select SoldAsVacant,
 		When SoldAsVacant ='n' THEN 'No'
 		else SoldAsVacant
 		END
-From dbo.NashvileHousing
+From dbo.NashvilleHousing
 
-Update NashvileHousing
+Update NashvilleHousing
 Set SoldAsVacant = CASE
 		When SoldAsVacant ='Y' THEN 'Yes'
 		When SoldAsVacant ='N' THEN 'No'
@@ -135,7 +135,7 @@ Select*,
 				ORDER BY UniqueID)
 				row_num
 
-From dbo.NashvileHousing
+From dbo.NashvilleHousing
 
 )
 Select *
@@ -148,9 +148,9 @@ where row_num >1
 --Delete Unused Columns
 
 Select *
-from dbo.NashvileHousing
+from dbo.NashvilleHousing
 
-ALTER TABLE dbo.NashvileHousing
+ALTER TABLE dbo.NashvilleHousing
 DROP COLUMN OwnerAddress, SaleDate, TaxDistrict, PropertyAddress
 
 
